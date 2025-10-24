b81edd6ce1257c2d02994b0081519c4e3ddec653
Arthur <arthurzhm2@gmail.com>
2025-10-24 16:40:56 -0300

    feat: shadcn installation

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,15 @@
+import chess_pieces from '../assets/images/chess-pieces.png'
+
 export default function HomePage() {
     return (
-        <div>
-            
+        <div className="bg-[#302e2b] min-h-screen flex items-center justify-center">
+            <img src={chess_pieces} alt="Chess Pieces" className="w- h-auto" />
+            <div>
+                <span className='text-4xl text-white font-bold'>Informe seu username</span>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
